f4f2aa296e9a9be26490c2c5109ba020e3eabb80
Tommy Seamans <tlseamans1214@gmail.com>
2024-11-22 22:08:23 -0800

    Destroy obstacles offscreen and loop music

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -24,5 +24,9 @@ public class Obstacle : MonoBehaviour
 
     public virtual void Move() {
         transform.Translate(Vector3.forward * Time.deltaTime * verticleSpeed);
+
+        if (transform.position.z < -1000) {
+            Destroy(gameObject);
+        }
     }
 }

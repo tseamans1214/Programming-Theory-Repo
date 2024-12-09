426fe8a5cdfb5094255b119d4c058b315c70ac2e
Tommy Seamans <tlseamans1214@gmail.com>
2024-12-09 11:55:38 -0800

    Add highscores to database

--- Assets/Scripts/LeaderboardDB.cs
+++ Assets/Scripts/LeaderboardDB.cs
@@ -8,13 +8,21 @@ using System;
 
 public class LeaderboardDB : MonoBehaviour
 {
+    public static LeaderboardDB Instance;
     private MongoClient client;
     private IMongoDatabase database;
 
-    void Start()
+    private void Awake()
     {
+        // To prevent multiple LeaderboardDB from being created, destroy it if it already exists
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
         ConnectToDatabase();
-        TestDatabaseConnection();
     }
 
     private void ConnectToDatabase()
@@ -36,6 +44,13 @@ public class LeaderboardDB : MonoBehaviour
         var document = new BsonDocument { { "name", "Unity User" }, { "age", 25 } };
         collection.InsertOne(document);
         Debug.Log("Document inserted into MongoDB!");
+    }
+
+    public static void AddPlayerScore(string playerName, int playerScore) {
+        var collection = Instance.database.GetCollection<BsonDocument>("highscores");
+        var document = new BsonDocument { { "name", playerName }, { "score", playerScore } };
+        collection.InsertOne(document);
+        Debug.Log("Document inserted into MongoDB highscores!");
     }
 
 }
